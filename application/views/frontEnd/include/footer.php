500ce6cbc30805ee6b600d35527f4e86dade8c34
Davitaridani <davitaridani@gmail.com>
2022-05-16 13:46:16 +0700

    Footer link

--- application/views/frontEnd/include/footer.php
+++ application/views/frontEnd/include/footer.php
@@ -1,104 +1,5 @@
 <!-- Footer -->
 <footer class="footer">
-	<!-- Footer Top -->
-	<!-- <div class="background-footer" style="background-image:url(assets/img/banner-2.jpg)"></div>
-	<div class=" footer-top">
-		<div class="container">
-			<div class="inner text-content">
-				<div class="block-items">
-					<div class="row">
-						<div class="col-md-4">
-							<div class="item-text">
-								<div class="text-title-foot">
-									<a href="/">
-										<h3>Dhewi <span>Lestari</span></h3>
-									</a>
-								</div>
-								<div class="text-desc-foot">
-									<p>Pesona Mutiara Tidar 2, Blok BA Nomor 20, Karang Tengah, Karangwidoro, Kec. Dau, Malang, Jawa Timur
-										65151
-										Email: contact@eloiseventplanner.com</p>
-								</div>
-								<div class="item-sosmed">
-									<ul>
-										<li class="nav-item">
-											<a href="https://web.facebook.com/profile.php?id=100007898836533" class="nav-link text-secondary"><span class="iconify" data-icon="entypo-social:facebook" data-width="20"></span>
-											</a>
-										</li>
-										<li class="nav-item">
-											<a href="https://www.instagram.com/dewilestari_makeup_artist/?hl=en" class="nav-link text-secondary"><span class="iconify" data-icon="ri:instagram-fill" data-width="20"></span>
-											</a>
-										</li>
-										<li class="nav-item">
-											<a href="#" class="nav-link text-secondary"><span class="iconify" data-icon="iconoir:tiktok" data-width="20"></span>
-											</a>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="item-text">
-								<div class="text-title-foot">
-									<h3>Info Layanan</h3>
-								</div>
-								<div class="item-text-layan">
-									<ul>
-										<li class="nav-item">
-											<a href="" class="nav-link"> Paket Wedding</a>
-										</li>
-										<li class="nav-item">
-											<a href="" class="nav-link">Paket Prewedding</a>
-										</li>
-										<li class="nav-item">
-											<a href="" class="nav-link">Paket Dekorasi</a>
-										</li>
-										<li class="nav-item">
-											<a href="" class="nav-link">Paket Lamaran</a>
-										</li>
-										<li class="nav-item">
-											<a href="" class="nav-link">Paket Dokumentasi</a>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-3">
-							<div class="item-text">
-								<div class="text-title-infor">
-									<h3>Tentang Kami </h3>
-								</div>
-								<div class="text-infor">
-									<ul>
-										<li class="nav-item">
-											<a href="/" class="nav-link">Home</a>
-										</li>
-										<li class="nav-item">
-											<a href="" class="nav-link"> About</a>
-										</li>
-										<li class="nav-item">
-											<a href="" class="nav-link"> Paket</a>
-										</li>
-										<li class="nav-item">
-											<a href="" class="nav-link">Galeri</a>
-										</li>
-										<li class="nav-item">
-											<a href="" class="nav-link">ontact</a>
-										</li>
-										<li class="nav-item">
-											<a href="" class="nav-link"> Blog</a>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div> -->
-
-
 	<!-- Baru Footer Top -->
 	<div class="footer-top">
 		<div class="container-fluid">
@@ -114,12 +15,12 @@
 						<div class="icon-sosmed">
 							<ul>
 								<li>
-									<a href="">
+									<a href="https://www.instagram.com/dewilestari_makeup_artist/">
 										<span class="iconify" data-icon="ph:instagram-logo-bold"></span>
 									</a>
 								</li>
 								<li>
-									<a href="">
+									<a href="https://www.facebook.com/profile.php?id=100007898836533">
 										<span class="iconify" data-icon="bxl:facebook"></span>
 									</a>
 								</li>
@@ -129,8 +30,8 @@
 									</a>
 								</li>
 								<li>
-									<a href="">
-										<span class="iconify" data-icon="ci:twitter"></span>
+									<a href="https://www.youtube.com/channel/UCbq6uXegumlantSNgvTXeyw">
+										<span class="iconify" data-icon="tabler:brand-youtube"></span>
 									</a>
 								</li>
 							</ul>
