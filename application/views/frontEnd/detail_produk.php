63279b9a6f92e36b561015529ea42b2ce3a1ede2
Davitaridani <davitaridani@gmail.com>
2022-05-16 13:15:18 +0700

    detail produk

--- application/views/frontEnd/detail_produk.php
+++ application/views/frontEnd/detail_produk.php
@@ -19,76 +19,85 @@
 <!-- Detail produk sect 2 -->
 <section class="detailProduk-sect-2">
 	<div class="container">
-		<div class="inner text-content">
-			<div class="blocks-items">
-				<!-- <h3><?= $produk->nama_kategori ?></h3> -->
-				<div class="row">
-					<div class="col-md-7 p-0">
-						<div class="owl-carousel owl-theme owl-lazy mx-auto" id="owl-img-detailProduk">
-							<?php foreach ($foto as $key => $value) { ?>
-								<div class="item">
-									<div class="logo">
-										<img src="<?= base_url('assets/foto_produk/' . $value->foto) ?>" alt="">
+		<div class="blocks-items">
+			<!-- <h3><?= $produk->nama_kategori ?></h3> -->
+			<div class="row">
+				<div class="col-md-7 p-0">
+					<div class="owl-carousel owl-theme owl-lazy mx-auto" id="owl-img-detailProduk">
+						<?php foreach ($foto as $key => $value) { ?>
+							<div class="item">
+								<div class="logo">
+									<img src="<?= base_url('assets/foto_produk/' . $value->foto) ?>" alt="">
+								</div>
+							</div>
+						<?php } ?>
+					</div>
+				</div>
+
+				<div class="col-md-5">
+					<div class="items-text">
+						<div class="title">
+							<h3><?= $produk->nama_produk ?></h3>
+						</div>
+						<div class="text">
+							<div class="price">
+								<p>Rp. <?= number_format($produk->harga, 0) ?></p>
+							</div>
+							<div class="desc">
+								<p><?= $produk->deskripsi ?></p>
+							</div>
+
+							<?php
+							echo form_open('belanja/add');
+							echo form_hidden('id', $produk->id_produk);
+							echo form_hidden('price', $produk->harga);
+							echo form_hidden('name', $produk->nama_produk);
+							echo form_hidden('redirect_page', str_replace('index.php/', '', current_url()));
+							?>
+							<div class="row">
+								<div class="col-md-3">
+									<div class="item-qty">
+										<input type="number" name="qty" min="1" max="1" value="1" class="form-control">
 									</div>
 								</div>
-							<?php } ?>
+								<div class="col-md-5">
+									<div class="item-addCart">
+										<button type="submit" id="btnSweetAlert" class="nav-link p-0">
+											Add Cart
+										</button>
+									</div>
+								</div>
+								<div class="item-meta-kat">
+									<h5>Kategori: <a href="<?= base_url() ?>"> <?= $produk->nama_kategori ?></a>
+									</h5>
+								</div>
+							</div>
+							<?php form_close(); ?>
 						</div>
 					</div>
-
-					<div class="col-md-5">
-						<div class="items-text">
-							<div class="title">
-								<h3><?= $produk->nama_produk ?></h3>
-							</div>
-							<div class="text">
-								<div class="price">
-									<p>Rp. <?= number_format($produk->harga, 0) ?></p>
-								</div>
-								<div class="desc">
-									<p><?= $produk->deskripsi ?></p>
-								</div>
-
-								<?php
-								echo form_open('belanja/add');
-								echo form_hidden('id', $produk->id_produk);
-								echo form_hidden('price', $produk->harga);
-								echo form_hidden('name', $produk->nama_produk);
-								echo form_hidden('redirect_page', str_replace('index.php/', '', current_url()));
-								?>
-								<div class="row">
-									<div class="col-md-3">
-										<div class="item-qty">
-											<input type="number" name="qty" min="1" max="1" value="1" class="form-control">
-										</div>
-									</div>
-									<div class="col-md-5">
-										<div class="item-addCart">
-											<button type="submit" id="btnSweetAlert" class="nav-link p-0">
-												Add Cart
-											</button>
-										</div>
-									</div>
-									<div class="item-meta-kat">
-										<h5>Kategori: <a href="<?= base_url() ?>"> <?= $produk->nama_kategori ?></a>
-										</h5>
-									</div>
-								</div>
-
-								<?php form_close(); ?>
-
-							</div>
-						</div>
-					</div>
+				</div>
+				<div class="block-spek-produk">
 					<div class="row">
 						<div class="title">
-							<h3>Keterangan</h3>
+							<h3>Keterangan detail produk</h3>
 						</div>
-						<div class="col-md-8 offset-lg-3">
-							<div class="desc">
-								<p><?= $produk->spesifikasi_produk ?></p>
+						<div class="text-detail-produk">
+
+							<div class="col-md-4">
+								<div class="desc">
+									<p><?= $produk->spesifikasi_produk ?></p>
+								</div>
 							</div>
 						</div>
 					</div>
+				</div>
+			</div>
+			<div class="item-note">
+				<div class="text">
+					<p>Note :</p>
+					<p>Harga harga di atas belum termasuk biasa transport.<br>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa, soluta.</p>
+
 				</div>
 			</div>
 		</div>
