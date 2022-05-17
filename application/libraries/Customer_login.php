797230fff7aee19950ef40cc20053ea7c459cdab
Davitaridani <davitaridani@gmail.com>
2022-05-17 16:24:26 +0700

    fix customer login

--- application/libraries/Customer_login.php
+++ application/libraries/Customer_login.php
@@ -18,11 +18,15 @@ class Customer_login
 	{
 		$cek = $this->ci->m_auth->login_customer($email, $password);
 		if ($cek) {
+			$id_customer = $cek->id_customer;
 			$nama_customer = $cek->nama_customer;
 			$email = $cek->email;
+			$foto = $cek->foto;
 			// Bat Sesseion
+			$this->ci->session->set_userdata('id_customer', $id_customer);
 			$this->ci->session->set_userdata('nama_customer', $nama_customer);
 			$this->ci->session->set_userdata('email', $email);
+			$this->ci->session->set_userdata('foto', $foto);
 			redirect('home');
 		} else {
 			// Jika Password Slah
@@ -41,8 +45,10 @@ class Customer_login
 
 	public function logout()
 	{
+		$this->ci->session->unset_userdata('id_customer');
 		$this->ci->session->unset_userdata('nama_customer');
 		$this->ci->session->unset_userdata('email');
+		$this->ci->session->unset_userdata('foto');
 		$this->ci->session->set_flashdata('pesan', 'Anda Berhasil Logout.');
 		redirect('customer/login');
 	}
